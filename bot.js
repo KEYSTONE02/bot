aea4ede4528c97d770de80753248922b94b13b08
samet99 <41552216+samet99@users.noreply.github.com>
2018-07-29 20:18:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,48 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('./ayarlar.json');
+
+var prefix = ayarlar.prefix;
 
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`BOT: ${client.user.username} adı ile giriş yaptı!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'hello') {
-    	message.reply('iyi senden naber');
+client.on('message', msg => {
+  console.log(`LOG: S: ${msg.guild.name} M: ${msg.content} Y: ${msg.author.tag}`);
+  if (msg.author.id === ayarlar.id) return;
+  if (msg.author.bot) return;
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
+  if (!msg.content.startsWith(prefix)) {
+	  return;
+  }
+  if (msg.content.toLowerCase() === prefix + 'ping') {
+    msg.reply('Pong! **' + client.ping + '** ms');
+  }
+  if (msg.content.toLowerCase() === prefix + 'sa') {
+    msg.reply('Aleyküm selam!');
+  }
+  if (msg.content.toLowerCase() === prefix + 'yaz') {
+    msg.delete();
+    msg.channel.sendMessage(msg.content);
+  }
+  if (msg.content.toLowerCase() === prefix + 'temizle') {
+    msg.channel.bulkDelete(100);
+    msg.channel.sendMessage("100 adet mesaj silindi!");
+  }
+  if (msg.content.toLowerCase() === prefix + 'reboot') {
+    if (msg.author.id !== ayarlar.sahip) {
+      msg.reply('Benim yapımcım değilsin!');
+    } else {
+      msg.channel.sendMessage(`Bot yeniden başlatılıyor...`).then(msg => {
+      console.log(`BOT: Bot yeniden başlatılıyor...`);
+      process.exit(0);
+    })
+   }
+  }
+});;
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
